fe555ad9628fc98a319c32b2eaedc552fe7e0e0e
Piotr <noschematic@gmail.com>
2018-01-22 22:39:31 +0100

    Enable cors and test it with axios.

--- AwesomeApp.ReactApp/todos/src/containers/toDoList/ToDoList.js
+++ AwesomeApp.ReactApp/todos/src/containers/toDoList/ToDoList.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 
 import AdTask from '../../components/AdTask/AdTask';
+
+axios.get('http://localhost:59967/api/todos')
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 class ToDoList extends Component {
   render () {
